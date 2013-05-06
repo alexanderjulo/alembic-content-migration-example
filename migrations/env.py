6a0e22f8f85194e889d14061544712dad36f330a
Alexander Jung-Loddenkemper <alexander.julo@googlemail.com>
2013-05-06 14:51:06 +0200

    import and use the new model in alembic.

--- migrations/env.py
+++ migrations/env.py
@@ -2,6 +2,14 @@ from __future__ import with_statement
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
+import os
+import sys
+
+# append current path to get the app
+sys.path.append(os.getcwd())
+
+# get the app
+import database
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -15,7 +23,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = database.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
