0ce59f03dd4c9f6feb820d416e1087e8000ef21e
Alexander Jung-Loddenkemper <alexander.julo@googlemail.com>
2013-05-06 15:21:58 +0200

    add a routine for creating example data.

--- database.py
+++ database.py
@@ -14,3 +14,21 @@ class Contact(Base):
 
     def __repr__(self):
         return '<Contact: %s>' % self.name
+
+
+def create_example_contacts():
+    sballmer = Contact('Steve Ballmer')
+    sjobs = Contact('Steve Jobs')
+    mzuckerberg = Contact('Mark Zuckerberg')
+
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    engine = create_engine(
+        'mysql://alexander:@localhost/alembic_content_migration_example'
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.add(sballmer)
+    session.add(sjobs)
+    session.add(mzuckerberg)
+    session.commit()
